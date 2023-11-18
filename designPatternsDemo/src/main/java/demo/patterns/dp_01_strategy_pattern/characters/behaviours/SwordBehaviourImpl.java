e5e3db1d3a9343cb31751f884df505b6c1011472
RosenMitrov <rdatabase.m@gmail.com>
2023-11-18 13:49:06 +0200

    added concrete implementation of sword behaviour

---
+++ designPatternsDemo/src/main/java/demo/patterns/dp_01_strategy_pattern/characters/behaviours/SwordBehaviourImpl.java
@@ -0,0 +1,11 @@
+package demo.patterns.dp_01_strategy_pattern.characters.behaviours;
+
+import demo.patterns.dp_01_strategy_pattern.characters.WeaponBehaviour;
+
+public class SwordBehaviourImpl implements WeaponBehaviour {
+
+    @Override
+    public void useWeapon() {
+        System.out.println("I am using my sward.");
+    }
+}
