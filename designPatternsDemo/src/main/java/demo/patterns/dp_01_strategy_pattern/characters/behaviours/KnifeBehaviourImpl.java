ab35a4e4fc72351c3b95ca80b424d4a18bc71a49
RosenMitrov <rdatabase.m@gmail.com>
2023-11-18 14:01:18 +0200

    changed directory of concrete implementation of knife behaviour.

--- designPatternsDemo/src/main/java/demo/patterns/dp_01_strategy_pattern/characters/behaviours/KnifeBehaviourImpl.java
+++ designPatternsDemo/src/main/java/demo/patterns/dp_01_strategy_pattern/characters/behaviours/KnifeBehaviourImpl.java
@@ -1,4 +1,6 @@
-package demo.patterns.dp_01_strategy_pattern.characters;
+package demo.patterns.dp_01_strategy_pattern.characters.behaviours;
+
+import demo.patterns.dp_01_strategy_pattern.characters.WeaponBehaviour;
 
 public class KnifeBehaviourImpl implements WeaponBehaviour {
     
