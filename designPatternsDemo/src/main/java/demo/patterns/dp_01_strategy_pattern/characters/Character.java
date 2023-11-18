60aae080d81b7b971d01048b2a92a18ea28f69d9
RosenMitrov <rdatabase.m@gmail.com>
2023-11-18 14:23:50 +0200

    added setter in abstract class Character for changing the behaviour at runtime.

--- designPatternsDemo/src/main/java/demo/patterns/dp_01_strategy_pattern/characters/Character.java
+++ designPatternsDemo/src/main/java/demo/patterns/dp_01_strategy_pattern/characters/Character.java
@@ -2,5 +2,10 @@ package demo.patterns.dp_01_strategy_pattern.characters;
 
 public abstract class Character {
     protected WeaponBehaviour weaponBehaviour;
+
     abstract void fight();
+    
+    public void setWeaponBehaviour(WeaponBehaviour weaponBehaviour) {
+        this.weaponBehaviour = weaponBehaviour;
+    }
 }
