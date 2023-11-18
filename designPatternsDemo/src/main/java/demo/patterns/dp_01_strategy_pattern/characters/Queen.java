eaa7255961ad1d2143c27326143fb0fbe4bd9491
RosenMitrov <rdatabase.m@gmail.com>
2023-11-18 14:16:59 +0200

    added queen character.

---
+++ designPatternsDemo/src/main/java/demo/patterns/dp_01_strategy_pattern/characters/Queen.java
@@ -0,0 +1,9 @@
+package demo.patterns.dp_01_strategy_pattern.characters;
+
+public class Queen extends Character {
+
+    @Override
+    void fight() {
+        this.weaponBehaviour.useWeapon();
+    }
+}
